390122a673dba825240e51efb81f0c93b7e14b55
Vixent <danvixent@gmail.com>
2020-01-08 11:43:43 +0100

    Added files

---
+++ helpers.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"net/http"
+	//"mime"
+)
+
+func renderTempate(w *http.ResponseWriter, tmpl string, tmp *Store) {
+
+	t := template.New("json")
+
+	t, err := t.Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	if err := t.Execute(buf, *tmp); err != nil {
+		http.Error(*w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := *w
+	res.Write(buf.Bytes())
+	res.(http.Flusher).Flush()
+}
